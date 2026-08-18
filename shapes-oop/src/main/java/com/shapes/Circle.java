3943edce8f6bea8615e427cba3f9a8e3ff4d4970
adogins <124842665+adogins@users.noreply.github.com>
2026-08-18 12:04:46 -0400

    Implement Circle class with radius and super(name)

---
+++ shapes-oop/src/main/java/com/shapes/Circle.java
@@ -0,0 +1,32 @@
+package com.shapes;
+
+/**
+ * Represents a circle defined by it's radius.
+ */
+public class Circle extends Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        super("Circle");
+
+        if (radius <= 0) {
+            throw new IllegalArgumentException("Radius must be positive.");
+        }
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String describe() {
+        return super.describe() + "(radius = " + radius + ")";
+    }
+}
